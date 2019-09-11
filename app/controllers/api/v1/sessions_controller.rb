e701dc6527ea50eb016309917f83e195965edb9a
Zenjutahi <zenjutahi@gmail.com>
2019-09-11 14:06:56 +0300

    [feature] - configure user sign-out

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
-  before_action :load_user, only: :create 
+  before_action :load_user, only: :create
+  before_action :valid_token, only: :destory
+  skip_before_action :verify_signed_out_user, only: :destory
+  
   # sign in
   def create
     if @user.valid_password?(sign_in_params[:password])
@@ -9,7 +12,13 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response "Unauthorized", false, {}, :unauthorized
     end
-    
+  end
+  
+  # log out
+  def destory
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Log out Succesfully", true, {}, :ok
   end
   
   private
@@ -23,6 +32,16 @@ class Api::V1::SessionsController < Devise::SessionsController
       return @user
     else
       json_response "User doesn't exist", false, {}, :failure
+    end
+  end
+  
+  def valid_token
+    binding.pry
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {}, :failure
     end
   end
 end
